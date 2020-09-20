0473ce1555599aa6f40c4a1cfee42dd8589355c6
malikk1510 <69068011+malikk1510@users.noreply.github.com>
2020-09-20 15:20:51 +0530

    Updates

---
+++ precache-manifest.9583b790ba0ba4051271897ffe130f52.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab503425be10848aca346f146885c575",
+    "url": "/to-do/index.html"
+  },
+  {
+    "revision": "2fa72ff9becca61af807",
+    "url": "/to-do/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "da7143f35e69ad9e1518",
+    "url": "/to-do/static/css/main.bdabf054.chunk.css"
+  },
+  {
+    "revision": "2fa72ff9becca61af807",
+    "url": "/to-do/static/js/2.a9944b6c.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/to-do/static/js/2.a9944b6c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da7143f35e69ad9e1518",
+    "url": "/to-do/static/js/main.cb5772d1.chunk.js"
+  },
+  {
+    "revision": "48fd9203ae660de589e4",
+    "url": "/to-do/static/js/runtime-main.8516aa0c.js"
+  },
+  {
+    "revision": "d9dc449f80059fd03c1ad889d5ba7141",
+    "url": "/to-do/static/media/pexels-hristo-fidanov-1252869.d9dc449f.jpg"
+  }
+]);
